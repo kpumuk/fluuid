2f24e2130a3f460b39ad4c232a6716e4e8d4a489
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2021-08-03 21:48:30 -0400

    Switched pgx to a custom fork with Uuid type support (until PR merged), and added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-mod pgx_uuid;
 mod uuid_v6;
 
-use crate::pgx_uuid::Uuid;
 use pgx::*;
 use std::sync::atomic;
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -36,32 +34,48 @@ pub extern "C" fn _PG_init() {
 
 #[pg_extern]
 fn gen_uuid_v1() -> Uuid {
-    Uuid::new(::uuid::Uuid::new_v1(gen_timestamp(), NODE_ID.share().0.as_ref()).unwrap())
+    Uuid::from_bytes(
+        *::uuid::Uuid::new_v1(gen_timestamp(), NODE_ID.share().0.as_ref())
+            .unwrap()
+            .as_bytes(),
+    )
 }
 
 #[pg_extern]
 fn gen_uuid_v1_random_node() -> Uuid {
-    Uuid::new(::uuid::Uuid::new_v1(gen_timestamp(), &gen_random_node_id()).unwrap())
+    Uuid::from_bytes(
+        *::uuid::Uuid::new_v1(gen_timestamp(), &gen_random_node_id())
+            .unwrap()
+            .as_bytes(),
+    )
 }
 
 #[pg_extern]
 fn gen_uuid_v4() -> Uuid {
-    Uuid::new(::uuid::Uuid::new_v4())
+    Uuid::from_bytes(*::uuid::Uuid::new_v4().as_bytes())
 }
 
 #[pg_extern]
 fn gen_uuid_v6() -> Uuid {
-    Uuid::new(uuid_v6::new_v6(gen_timestamp(), NODE_ID.share().0.as_ref()).unwrap())
+    Uuid::from_bytes(
+        *uuid_v6::new_v6(gen_timestamp(), NODE_ID.share().0.as_ref())
+            .unwrap()
+            .as_bytes(),
+    )
 }
 
 #[pg_extern]
 fn gen_uuid_v6_random_node() -> Uuid {
-    Uuid::new(uuid_v6::new_v6(gen_timestamp(), &gen_random_node_id()).unwrap())
+    Uuid::from_bytes(
+        *uuid_v6::new_v6(gen_timestamp(), &gen_random_node_id())
+            .unwrap()
+            .as_bytes(),
+    )
 }
 
 #[pg_extern]
 fn gen_uuid_nil() -> Uuid {
-    Uuid::new(::uuid::Uuid::nil())
+    Uuid::from_bytes(*::uuid::Uuid::nil().as_bytes())
 }
 
 fn gen_timestamp() -> ::uuid::v1::Timestamp {
@@ -81,38 +95,118 @@ fn gen_random_node_id() -> [u8; 6] {
 
 #[cfg(any(test, feature = "pg_test"))]
 mod tests {
-    // use crate::*;
-    // use pgx::*;
+    const MAX_DIFF_NANOS: u64 = 1_000_000_000; // 1 second
 
-    // #[pg_test]
-    // fn test_hello_gen_random_uuid_nil() {
-    //     assert_eq!(uuid::Uuid::nil(), crate::gen_random_uuid_nil().0);
-    // }
-    //
-    // #[pg_extern]
-    // fn return_uuid() -> Uuid {
-    //     Uuid(uuid::Uuid::nil())
-    // }
+    use crate::*;
 
-    // #[pg_test]
-    // fn test_return_uuid() {
-    //     let uuid =
-    //         Spi::get_one::<Uuid>("SELECT tests.return_uuid();").expect("SPI result was null");
-    //     assert_eq!(uuid, Uuid(uuid::Uuid::nil()))
-    // }
-    //
-    // #[pg_test]
-    // fn test_return_null_uuid() {
-    //     let uuid = Spi::get_one::<Uuid>("SELECT NULL::uuid;");
-    //     assert_eq!(uuid, None)
-    // }
-    //
-    // #[pg_test]
-    // fn test_return_uuid_v4() {
-    //     let uuid =
-    //         Spi::get_one::<Uuid>("SELECT gen_random_uuid_v4();").expect("SPI result was null");
-    //     assert_eq!(uuid, Uuid(uuid::Uuid::nil()))
-    // }
+    macro_rules! assert_ts {
+        ($expression:expr) => {
+            if let Some(ts) = $expression {
+                let cur_ts = super::gen_timestamp();
+                assert!(
+                    cur_ts.to_unix_nanos() - ts.to_unix_nanos() <= MAX_DIFF_NANOS,
+                    "UUID timestamp should be within 1 second from now, but the actual difference is {}ns",
+                    cur_ts.to_unix_nanos() - ts.to_unix_nanos(),
+                );
+                assert_eq!(
+                    cur_ts.to_rfc4122().1 - ts.to_rfc4122().1,
+                    1,
+                    "clock sequence should increase by 1"
+                )
+            } else {
+                panic!("Expected timestamp, got None");
+            }
+        };
+    }
+
+    #[pg_test]
+    fn test_gen_uuid_v1() {
+        let res = Spi::get_one::<Uuid>("SELECT gen_uuid_v1();").expect("SPI result was null");
+        let uuid = ::uuid::Uuid::from_bytes(*res.as_bytes());
+
+        assert_eq!(uuid.get_version_num(), 1, "returns UUID v1");
+        assert_eq!(uuid.get_variant(), Some(::uuid::Variant::RFC4122));
+        assert_ts!(uuid.to_timestamp());
+
+        assert_eq!(
+            &uuid.as_bytes()[10..16],
+            NODE_ID.share().0.as_ref(),
+            "node ID should be shared for the process",
+        );
+    }
+
+    #[pg_test]
+    fn test_gen_uuid_v1_random_node() {
+        let res =
+            Spi::get_one::<Uuid>("SELECT gen_uuid_v1_random_node();").expect("SPI result was null");
+        let uuid = ::uuid::Uuid::from_bytes(*res.as_bytes());
+
+        assert_eq!(uuid.get_version_num(), 1, "returns UUID v1");
+        assert_eq!(uuid.get_variant(), Some(::uuid::Variant::RFC4122));
+        assert_ts!(uuid.to_timestamp());
+
+        assert_ne!(
+            &uuid.as_bytes()[10..16],
+            NODE_ID.share().0.as_ref(),
+            "node ID should be random",
+        );
+    }
+
+    #[pg_test]
+    fn test_gen_uuid_v4() {
+        let res = Spi::get_one::<Uuid>("SELECT gen_uuid_v4();").expect("SPI result was null");
+        let uuid = ::uuid::Uuid::from_bytes(*res.as_bytes());
+
+        assert_eq!(uuid.get_version_num(), 4, "returns UUID v4");
+        assert_eq!(uuid.get_variant(), Some(::uuid::Variant::RFC4122));
+
+        assert_ne!(
+            &uuid.as_bytes()[10..16],
+            NODE_ID.share().0.as_ref(),
+            "node ID should be shared for the process",
+        );
+    }
+
+    #[pg_test]
+    fn test_gen_uuid_nil() {
+        let res = Spi::get_one::<Uuid>("SELECT gen_uuid_nil();").expect("SPI result was null");
+        let uuid = ::uuid::Uuid::from_bytes(*res.as_bytes());
+
+        assert_eq!(uuid, ::uuid::Uuid::nil());
+    }
+
+    #[pg_test]
+    fn test_gen_uuid_v6() {
+        let res = Spi::get_one::<Uuid>("SELECT gen_uuid_v6();").expect("SPI result was null");
+        let uuid = ::uuid::Uuid::from_bytes(*res.as_bytes());
+
+        assert_eq!(uuid.get_version_num(), 6, "returns UUID v6");
+        assert_eq!(uuid.get_variant(), Some(::uuid::Variant::RFC4122));
+        // assert_ts!(uuid.to_timestamp());
+
+        assert_eq!(
+            &uuid.as_bytes()[10..16],
+            NODE_ID.share().0.as_ref(),
+            "node ID should be shared for the process",
+        );
+    }
+
+    #[pg_test]
+    fn test_gen_uuid_v6_random_node() {
+        let res =
+            Spi::get_one::<Uuid>("SELECT gen_uuid_v6_random_node();").expect("SPI result was null");
+        let uuid = ::uuid::Uuid::from_bytes(*res.as_bytes());
+
+        assert_eq!(uuid.get_version_num(), 6, "returns UUID v6");
+        assert_eq!(uuid.get_variant(), Some(::uuid::Variant::RFC4122));
+        // assert_ts!(uuid.to_timestamp());
+
+        assert_ne!(
+            &uuid.as_bytes()[10..16],
+            NODE_ID.share().0.as_ref(),
+            "node ID should be random",
+        );
+    }
 }
 
 #[cfg(test)]
@@ -123,6 +217,6 @@ pub mod pg_test {
 
     pub fn postgresql_conf_options() -> Vec<&'static str> {
         // return any postgresql.conf settings that are required for your tests
-        vec![]
+        vec!["shared_preload_libraries = 'fluuid'"]
     }
 }
